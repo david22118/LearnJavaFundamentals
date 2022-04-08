6bace08ebca866d18159de64dada0b655935ccf2
David_Ben_Yakov <david@getjones.com>
2022-04-08 23:00:35 +0300

    control flow

--- src/com/learnjava/Main.java
+++ src/com/learnjava/Main.java
@@ -1,30 +1,74 @@
 package com.learnjava;
 
 import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 
 public class Main {
 
     public static void main(String[] args) {
-/// First project
+/*
+        Second project
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        String principal = scanner.nextLine().trim();
+        System.out.print("Number: ");
+        String userInput = scanner.nextLine().trim();
 
-        System.out.print("Annual Interest Rate: ");
-        String annualInterestRate = scanner.nextLine().trim();
+        Integer userInputNumber = Integer.parseInt(userInput);
 
-        System.out.print("Period (Years): ");
-        String period = scanner.nextLine().trim();
+        if(userInputNumber % 5 == 0 && userInputNumber % 3 == 0 ){
+            System.out.println("FizzBuzz");
+        }
+        else if (userInputNumber % 5 == 0 ){
+            System.out.println("Fizz");
+        }
+        else if(userInputNumber % 3 == 0 ){
+            System.out.println("Buzz");
+        }
+        else{
+            System.out.println(userInputNumber);
+        }
+*/
 
-        Double principalNumber = Double.parseDouble(principal);
-        Double monthInterestRate = Double.parseDouble(annualInterestRate) / 100 / 12;
-        Double periodMonths = Double.parseDouble(period) * 12;
+        Scanner scanner = new Scanner(System.in);
 
-        Double mortgage = (principalNumber * (monthInterestRate * (Math.pow((1 + monthInterestRate), periodMonths)))) / ((Math.pow((1 + monthInterestRate), periodMonths)) - 1);
+        System.out.print("Principal (1K-1M): ");
+        Integer principal = scanner.nextInt();
+
+        while (principal < 1000 || principal > 1000000) {
+            System.out.println("Enter a number between 1,000 and 1,000,000.");
+            System.out.print("Principal (1K-1M): ");
+            principal = scanner.nextInt();
+        }
+
+        System.out.print("Annual Interest Rate (0-10): ");
+        Float annualInterestRate = scanner.nextFloat();
+
+        while (annualInterestRate <= 0 || annualInterestRate > 10) {
+            System.out.println("Enter a value greater than 0 and less then or equal to 10.");
+            System.out.print("Annual Interest Rate (0-10): ");
+            annualInterestRate = scanner.nextFloat();
+        }
+
+        System.out.print("Period (Years 1-30): ");
+        Integer period = scanner.nextInt();
+
+        while (period < 1 || period > 30) {
+            System.out.println("Enter a value between 1 to 30.");
+            System.out.print("Period (Years 1-30): ");
+            period = scanner.nextInt();
+        }
+
+
+        Float monthInterestRate = annualInterestRate / 100 / 12;
+        Integer periodMonths = period * 12;
+
+        double mortgage = (principal * (monthInterestRate * (Math.pow((1 + monthInterestRate), periodMonths)))) / ((Math.pow((1 + monthInterestRate), periodMonths)) - 1);
 
         System.out.println(NumberFormat.getCurrencyInstance().format(mortgage));
+
+
     }
 }
